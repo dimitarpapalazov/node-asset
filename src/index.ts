dc3b70421a77d1b40184052138faa30869f9d944
dimitarpapalazov <d.papalazov@outlook.com>
2026-05-21 18:18:26 +0200

    feat(api): add express and health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world")
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).json({ status: 'UP' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
