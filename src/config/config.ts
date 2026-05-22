1b92247c008877b339aa9b7c112796fd10d5de6e
dimitarpapalazov <d.papalazov@outlook.com>
2026-05-22 17:45:09 +0200

    feat: initialize drizzle orm with postgres configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -11,13 +11,39 @@ export interface Config {
      * The port the server should listen on.
      */
     port: number;
+
+    /**
+     * Database configuration.
+     */
+    db: {
+        host: string;
+        port: number;
+        user: string;
+        pass: string;
+        name: string;
+        url: string;
+    };
 }
+
+const dbUser = envOrThrow('DB_USER');
+const dbPass = envOrThrow('DB_PASSWORD');
+const dbHost = envOrThrow('DB_HOST');
+const dbPort = envOrThrow('DB_PORT');
+const dbName = envOrThrow('DB_NAME');
 
 /**
  * Global configuration object.
  */
 export const config: Config = {
     port: parseInt(envOrThrow('PORT'), 10),
+    db: {
+        host: dbHost,
+        port: parseInt(dbPort, 10),
+        user: dbUser,
+        pass: dbPass,
+        name: dbName,
+        url: `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`,
+    },
 };
 
 /**
